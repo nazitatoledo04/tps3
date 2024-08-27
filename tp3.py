9c2c818fb9cc0d14038e74f3ad27ddba1b36deca
maxicantarella1 <maxicantarella1@gmail.com>
2024-08-27 20:29:38 -0300

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -13,8 +13,6 @@ def obtener_tipo_control(linea):
                 return "Soft Control"
         anterior = i
 
-    return "No anduvo"
-
 
 def es_mayuscula(car):
     return 'A' <= car <= 'Z'
@@ -24,13 +22,41 @@ def es_digito(car):
     return '0' <= car <= '9'
 
 
-def agregar_envio(vec):
-    codigo_postal = input("")
-    direccion_fis = input("")
-    tipo_envio = int(input())
-    forma_pago = int(input())
+def agregar_envio_manual(vec):
+    codigo_postal = input("Ingrese los 9 caracteres del Codigo Postal: ")
+    direccion_fis = input("Ingrese Los 20 caracteres de la Direccion Fisica Del Destino: ")
+    tipo_envio = int(input("Ingrese tipo de envio (Entre 0 y 6): "))
+    forma_pago = int(input("Ingrese Forma De Pago (1 o 2): "))
     envio = Envios(codigo_postal, direccion_fis, tipo_envio, forma_pago)
     vec.append(envio)
+    print("Envio Agregado con exitos!")
+
+
+def agregar_envio(vec):
+    archivo = open("envios-tp3.txt", "r")
+    m = archivo.read()
+    n_envios = 0
+    for i in archivo:
+        if n_envios >= 1:
+            cp = i[0:9].strip()
+            direccion = i[9:29].strip()
+            tipo = i[29].strip()
+            pago = i[30].strip()
+        for car in i:
+            es_digito(car)
+            es_mayuscula(car)
+
+        if n_envios == 0:
+            obtener_tipo_control(i)
+        n_envios += 1
+
+    codigo_postal = cp
+    direccion_fis = direccion
+    tipo_envio = tipo
+    forma_pago = pago
+    envio = Envios(codigo_postal, direccion_fis, tipo_envio, forma_pago)
+    vec.append(envio)
+    m.close
     print("Envio Agregado con exitos!")
 
 
@@ -49,42 +75,55 @@ def menu():
     print("2-Mostrar Envios")
     print("3-Buscar Envios")
     print("4-Eliminar Envio")
-    print("5-Salir")
-    opcion = int(input("Ingrese Opcion: "))
-    
+    print("5-punto 5")
+    print("6-punto 6")
+    print("7-punto 7")
+    print("8-punto 8")
+    print("9-punto 9")
+    print("0-Salir")
+    return int(input("Ingrese Opcion: "))
+
 
 def principal():
     vec = []
-    n_envios = 0
-    archivo = open("envios-tp3.txt", "r")
-    for i in archivo:
-        if n_envios >= 1:
-            cp = i[0:9].strip()
-            direccion = i[9:29].strip()
-            tipo = i[29].strip()
-            pago = i[30].strip()
-        for car in i:
-            es_digito(car)
-            es_mayuscula(car)
-            opcion= menu()
-            if opcion == 1:
-                pass
-            elif opcion == 2:
-                pass
-            elif opcion == 3:
-                pass
-            elif opcion == 4:
-                pass
-            elif opcion == 5:
-                break
-            else:
-                print("Opcion Invalida")
-        if n_envios == 0:
-            obtener_tipo_control(i)
-        n_envios += 1
-    print(n_envios)
+    opcion = 0
+    opcion = menu()
+    while opcion != 0:
 
-        
+        if opcion == 1:
+            if len(vec) > 0:
+                vec = agregar_envio(vec)
+            else:
+                eliminar_arreglo = input("Desea Eliminar el Arreglo? (S/N) ")
+                eliminar_arreglo.lower()
+                if eliminar_arreglo == "s":
+                    vec = agregar_envio_manual(vec)
+                elif eliminar_arreglo == "n":
+                    opcion = menu()
+
+        elif opcion == 2:
+            if len(vec) > 0:
+                mostrar_arreglo(vec)
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            pass
+        elif opcion == 6:
+            pass
+        elif opcion == 7:
+            pass
+        elif opcion == 8:
+            pass
+        elif opcion == 9:
+            pass
+        elif opcion == 0:
+            break
+        else:
+            print("Opcion Invalida")
+            opcion = menu()
+
 
 if __name__ == '__main__':
     principal()
