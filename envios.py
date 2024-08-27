3b4d53278ed846a1bbf1e687d329105b1d390df3
maxicantarella1 <maxicantarella1@gmail.com>
2024-08-27 18:22:58 -0300

    Create envios.py

---
+++ envios.py
@@ -0,0 +1,6 @@
+class Envios:
+    def __init__(self,codigo_postal,direccion_fis,tipo_envio,forma_pago):
+        self.codigo_psotal = codigo_postal
+        self.direccion_fis = direccion_fis
+        self.tipo_envio = tipo_envio
+        self.forma_pago = forma_pago
